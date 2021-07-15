e3bd04768c8ef690facf08a92b613013387a3ae6
Venkatesan <venkatesanabcd@gmail.com>
2021-07-15 19:43:43 +0530

    Update settings.py

--- budget/settings.py
+++ budget/settings.py
@@ -76,9 +76,12 @@ WSGI_APPLICATION = 'budget.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "icarus",
+        "USER": "icarusdbuser",
+        "HOST": "localhost",
+        "PASSWORD": "bipbipbip",
     }
 }
 
