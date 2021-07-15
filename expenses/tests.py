19f469db118fadb0d9cf0a3b9a4a820bd8981c02
Venkatesan <venkatesanabcd@gmail.com>
2021-07-15 18:22:07 +0530

    Update tests.py

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+import unittest
+from django.test import Client
+
+class SimpleTest(unittest.TestCase):
+    def test_details(self):
+        client = Client()
+#         response = client.get('/customer/details/')
+#         self.assertEqual(response.status_code, 200)
+
+    def test_index(self):
+        client = Client()
+#         response = client.get('/customer/index/')
+#         self.assertEqual(response.status_code, 200)
