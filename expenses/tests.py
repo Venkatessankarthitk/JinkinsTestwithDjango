d145fed348ca95f3ff0080b3276c863427923e78
Venkatesan <venkatesanabcd@gmail.com>
2021-07-15 18:25:18 +0530

    Update tests.py

--- expenses/tests.py
+++ expenses/tests.py
@@ -7,10 +7,10 @@ from django.test import Client
 class SimpleTest(unittest.TestCase):
     def test_details(self):
         client = Client()
-#         response = client.get('/customer/details/')
-#         self.assertEqual(response.status_code, 200)
+        response = client.get('/purchase_items/')
+        self.assertEqual(response.status_code, 200)
 
     def test_index(self):
         client = Client()
-#         response = client.get('/customer/index/')
-#         self.assertEqual(response.status_code, 200)
+        response = client.get('purchase_items')
+        self.assertEqual(response.status_code, 200)
